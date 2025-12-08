5ec4ce780f5062d3c99df1bbed73e24b1c68a335
schu1na <domschuinao@gmail.com>
2025-12-08 15:36:33 -0300

    feat:  Criação da página de explicação do Pagerank e adição do botão para essa página na Home Page

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home/home.page';
 import { GraphPage } from './pages/graph/graph.page';
+import { PagerankPage } from './pages/pagerank/pagerank.page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'graph', component: GraphPage },
+  { path: 'pagerank', component: PagerankPage },
   { path: '**', redirectTo: '' },
 ];
---
+++ front-end/src/app/pages/pagerank/pagerank.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-pagerank-page',
+  templateUrl: './pagerank.page.html',
+  styleUrls: ['./pagerank.page.scss'],
+  imports: [RouterModule],
+  standalone: true
+})
+export class PagerankPage {}
